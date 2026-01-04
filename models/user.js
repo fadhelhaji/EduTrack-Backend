f55977187fb3ebda21b20bdd380c9bb06a3dc8d4
Fadhel Haji <fadhelmh@hotmail.com>
2026-01-04 22:23:41 +0300

    added user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+// we need mongoose schema
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+  type: String,
+  enum: ['user', 'admin'],
+  default: 'user',
+}
+});
+// then we register the model with mongoose
+const User = mongoose.model('User', userSchema);
+
+// export the model
+module.exports = User;
