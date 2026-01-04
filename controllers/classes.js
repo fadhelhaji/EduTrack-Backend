f941f527803f38ec2f0d9b848f0ddd7a953bd708
Fadhel Haji <fadhelmh@hotmail.com>
2026-01-04 22:27:07 +0300

    added classes controller

---
+++ controllers/classes.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const Classes = require("../models/Classes");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const newClass = await Classes.create(req.body);
+    res.status(201).json({ class: newClass });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Failed to create class" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const classes = await Classes
+      .find({})
+      .populate("instructor")
+      .populate("student");
+
+    res.status(200).json({ classes });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Failed to get classes" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const singleClass = await Classes
+      .findById(id)
+      .populate("instructor") // remember that scary line in the model ? this gets the whole user object to display it in the front end 
+      .populate("student");
+
+    if (!singleClass) {
+      res.status(404).json({ err: "Class not found" });
+    } else {
+      res.status(200).json({ class: singleClass });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Failed to get class" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedClass = await Classes.findByIdAndDelete(id);
+
+    if (!deletedClass) {
+      res.status(404).json({ err: "Class not found" });
+    } else {
+      res.status(200).json({ class: deletedClass });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Failed to delete class" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedClass = await Classes.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updatedClass) {
+      res.status(404).json({ err: "Class not found" });
+    } else {
+      res.status(200).json({ class: updatedClass });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ err: "Failed to update class" });
+  }
+});
+
+module.exports = router;
