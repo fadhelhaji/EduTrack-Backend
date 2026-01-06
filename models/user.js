e415f32d23f62a5cea4d5332e8fc630a8cc058df
fatemamoh <fatimabuzuhaira@gmail.com>
2026-01-06 13:10:02 +0300

    user schema

--- models/user.js
+++ models/user.js
@@ -11,6 +11,17 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+
+  firstName:{
+    type: String,
+    required: true,
+  },
+
+  lastName:{
+    type: String,
+    required: true,
+  },
+
   role: {
     type: String,
     enum: ['Student', 'Instructor'],   
